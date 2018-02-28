8a4d37ae20a2626d356b7e5e80973bcea70ee7d0
yohanessahrul <yohanessahrul92@gmail.com>
2018-02-28 14:33:22 +0700

    app.js

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
 const express = require('express');
 const routerIndex = require('./routes/index');
+const routerMenu = require('./routes/menu');
+const bodyParser = require('body-parser');
 
 const app = express();
 
-app.use('/', routerIndex)
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
 
+app.set('view engine', 'ejs');
+
+app.use(express.static('public'));
+
+// ROUTES
+app.use('/', routerIndex);
+app.use('/menu', routerMenu);
 
 
 app.listen(3000, function(err){
